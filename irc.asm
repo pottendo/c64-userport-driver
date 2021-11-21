586a05cfd16384223e50627f4651228ac7b4423d
pottendo <pottendo@gmx.net>
2021-11-21 20:17:54 +0100

    improved msg buf

--- irc.asm
+++ irc.asm
@@ -3,12 +3,16 @@
 .namespace irc {
 seperator:  .text "----------------------------------------"
             .byte $00
-    .label inputaddr = $0400 + 23*40
+.label inputaddr = $0400 + 23*40
 crs:        .byte $00
-    .label msgbuffer = $0400
+.label rcvbuffer = $0400
+.label msgbuf  = $0400 + 19*40 //$9000
+.label msgptr = $14
+
 setup:
     jsr parport.arm_msgcnt  // launch NMI counter for pending msgs
     jsr STD.CLSCR
+    poke16_(msgptr, msgbuf)
     wstring(0, 22, seperator)
     poke8_(crs, 0)
     memset(inputaddr, ' ', 80)
@@ -43,25 +47,44 @@ inputloop:
     rts
 
 store_input:
-    memcpy_d(msgbuffer + 80, msgbuffer, 80 * 5) 
+    memcpy_d(rcvbuffer + 80, rcvbuffer, 80 * 5) 
 
     ldx crs
-    stx msgbuffer
+    stx rcvbuffer
 !:  lda inputaddr,x
-    sta msgbuffer + 1, x 
+    sta rcvbuffer + 1, x 
     dex
     bpl !-
     rts
 
 rcv_msgs:
     lda parport.pinput_pending
-    beq out
-    inc VIC.BoC
-    poke8_(msgbuffer + 1, 0)                // high byte 0, to support 16 bit len in addr - ugly misuse but efficient
-    uport_read_(msgbuffer, 1)
-    uport_read(msgbuffer + 1, msgbuffer)    // len is stored here
+    bne !+
+    rts
+!:
+    poke8_(rcvbuffer + 1, 0)                // high byte 0, to support 16 bit len in addr - ugly misuse but efficient
+    uport_read_(rcvbuffer, 1)
+    uport_read(rcvbuffer + 1, rcvbuffer)    // len is stored here
     dec parport.pinput_pending
     jsr parport.arm_msgcnt
+    ldy rcvbuffer
+!:  lda rcvbuffer, y
+    sta (msgptr), y
+    dey
+    bne !-
+    lda rcvbuffer   // store length
+    sta (msgptr), y
+    clc
+    adc #$01        // byte for len
+    clc 
+    adc msgptr
+    sta msgptr
+    lda #$00
+    adc msgptr + 1
+    cmp16_(msgptr, msgbuf + 20)
+    bcc out
+    inc VIC.BoC
+    poke16_(msgptr, msgbuf)
 out:
     rts
 }
