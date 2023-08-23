4c097493da0d6d1bbcbd1f9a103bbf60349a6d83
pottendo <pottendo@gmx.net>
2023-08-23 16:29:34 +0200

    irc stability

--- irc.asm
+++ irc.asm
@@ -1,5 +1,7 @@
 #import "pottendos_utils.asm"
 
+#define HANDLE_MEM_BANK // must be set to enable proper handling in userport-drv along soft80
+
 .namespace irc {
 crs:        .byte $00
 inputrow:   .byte 23
@@ -32,6 +34,9 @@ _t2:        .byte $00
 .label msgptr = $14
 .label currptr = $16
 .label newentrypos = $0400 + 17 * 40
+.label ribuf = $ce00    // 1 page ringbuffer for loop read
+.label rtail = $029b
+.label rhead = $029c
 
 setup:
 #if !TEST_IRC
@@ -55,6 +60,13 @@ setup:
     soft80_wstring(0, 22, seperator)
     soft80_pos_(0, 23)
 #endif
+
+    poke16_(parport.rt1 + 1, rtail)       // modify rtail operand for loopread
+    poke16_(parport.rt2 + 1, ribuf)
+    poke16_(parport.rt3 + 1, rtail)
+    poke16_(parport.rt4 + 1, rhead)
+    uport_lread(ccgms.ribuf)              // activate background read
+
     jsr inputloop
     jsr soft80.soft80_shutdown
     jmp main_.cmd4
@@ -149,7 +161,6 @@ store_input:
     bpl !-
 #if !TEST_IRC
     jsr send_msg
-    jsr parport.arm_msgcnt
 #endif
     memcpy_d(rcvbuffer + 16, rcvbuffer, 80)
     memcpy(rcvbuffer + 1, nick, 16)
@@ -162,20 +173,22 @@ store_input:
 
 #if !TEST_IRC
 rcv_msgs:
-    lda parport.pinput_pending
+    poke8_(rcvbuffer, 0)
+    jsr irc_in
     bne !+
     rts
 !:
-    ldx #1
-    uport_sread_f(rcvbuffer)
+    // input has arrived, read until end-marker $ff
+    beq empty_read+
+    cmp #$ff
+    beq cpy2msgbuf
+    inc rcvbuffer
     ldx rcvbuffer
-    cmp #81                 
-    bcc !+                 // larger tan 80
-    ldx #80                // truncate to 80
-    inc VIC.BoC
-!:  uport_sread_f(rcvbuffer + 1)
-    dec parport.pinput_pending
-    jsr parport.arm_msgcnt
+    sta rcvbuffer, x
+empty_read:
+    jsr irc_in
+    jmp !-
+
 #endif
 
 cpy2msgbuf:
@@ -300,8 +313,57 @@ send_msg:
     dey
     bne !-
     uport_write(cmd_args, tlen)
+    jsr parport.start_isr       // back to reading
     rts
 #endif
+
+irc_in:
+    jsr rsgetxfer
+	bcs !+              // if no character, then return 0 in a
+    rts
+!:	clc
+    lda #0
+    rts
+rsgetxfer:
+	ldx rhead
+    cpx rtail
+    beq !empty+             // skip (empty buffer, return with carry set)
+    lda ribuf,x
+	pha
+    inx
+    stx rhead
+    lda rtail
+    sec
+    sbc rhead
+/*  orig code, wrong IMHO
+    txa
+    sec
+    sbc ccgms.rtail
+*/
+    cmp #24
+    bcc !+  
+    clc 
+    pla
+!empty:
+    rts
+!:
+#if HANDLE_MEM_BANK
+        sei
+        lda     $01
+        pha
+        lda     #$37
+        sta     $01          
+#endif
+    poke8_(VIC.BoC, YELLOW)
+    clearbits(CIA2.PORTA, %11111011)   // clear PA2 to low to signal we're ready to receive again
+#if HANDLE_MEM_BANK
+        pla
+        sta $01
+        cli
+#endif
+    clc
+	pla
+    rts 
 
 }
 
