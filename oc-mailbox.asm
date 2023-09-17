4d6f744af2283445680aac313ec1dda0bd6ab8ed
pottendo <pottendo@gmx.net>
2023-09-17 12:43:29 +0200

    improvements

--- oc-mailbox.asm
+++ oc-mailbox.asm
@@ -15,7 +15,7 @@ BasicUpstart2(main_entry)
 
 .label oc_shm       = $dec0
 .label oc_interrupt = $defb
-.label oc_triggeroc = $defc
+.label oc_triggeroc = $deff
 .label coproc = oc_shm
 
 .const CLINE    = 1
@@ -29,7 +29,7 @@ ctrl_save:  .byte $00
 tmp1:       .byte $00
 deltmp:     .byte $00
 startval:   .byte $00
-crsync:     .byte $00
+crsync:     .byte $01
 
 .macro enable_roms()
 { 
@@ -45,7 +45,6 @@ crsync:     .byte $00
 {
     sei
     lda ctrl_save
-    sta $01
     poke16_($fffe, STD.NMI)
     poke8_(CIA1.ICR, $ff)           // enable all interrupts again
     cli
@@ -54,45 +53,51 @@ crsync:     .byte $00
 .macro wait4cr()
 {
 !:
-    lda VIC.BoC
-    lda crsync
+    inc VIC.BoC         // give room by accessing I/O and see activity
+    inc VIC.BoC
+    inc VIC.BoC
+    lda crsync          // poll the sync variable
     beq !-
 }
+
 .macro trigger_oc()
 {
-//    poke8_(oc_triggeroc, $ff);
-    poke8_(crsync, 0);
-    //poke16_(oc_triggeroc, $ffff)
-    poke8_(oc_triggeroc+3, $ff)
-    wait4cr()
+    poke8_(crsync, 0)
+    //dec crsync
+    poke8_(oc_triggeroc, $ff)   // trigger OCs ISR
+    wait4cr()   // if coproc is still working, we block here, busy waiting
 }
 
 oc_req:
-    pha
-    lda oc_interrupt
-    inc crsync
-    pla
+    lda oc_interrupt    // the oc driver triggers by writing $ff to trigger
+    bpl !+              
+    inc oc_interrupt    // why needed to ack the interrupt? read should be sufficient
+    inc crsync          // this can be polled to syncronize with finish of a co-routine
+    //inc VIC.BgC         // show something by setting background color
+    restore_regs()      // restore registers as needed for proper ISRs
+    rti
+!:
     jmp STD.IRQ
 
 do_test:
-    poke8_(P.zpp1 , 0)
-    ldy #$20
+    poke8_(oc_shm + 1, $fe) // $fe at oc_shm + 1 requests the test coroutine
+    poke8_(P.zpp1 , 0)      // P.zzp1 is a zeropage tmp address
+    ldy #$20                // loop 32x
 
 !next:
-    ldx #$3b
+    ldx #$3a                // fill the entire mailbox shm
 !:  
     txa
     clc 
-    adc P.zpp1
-    sta oc_shm, x
+    adc P.zpp1              // add something
+    sta oc_shm, x           // put in in the oc_shm
     dex
     bne !-
-    stx oc_shm
+    stx oc_shm              // also fill byte 0 (always with #0)
+    poke8_(oc_shm + 1, $fe) // request cr-test, as oc_shm + 1 was overritten
+    trigger_oc()            // trigger oc by issuing an interrupt and busy wait for finish
 
-    poke8_(oc_shm + 1, $fe) // request cr-test
-    trigger_oc()
-
-    inc P.zpp1
+    inc P.zpp1              // change for the next iteration
     dey
     bne !next-
 
@@ -105,19 +110,20 @@ delay:
     bne !-
     rts
 
-do_circles:
+do_circles_s:
 
     // lines
-    poke8_(deltmp, 40)   // iterator counter
+    poke8_(deltmp, 1)   // iterator counter
     poke16_(coproc+3, 10)
     poke8_(coproc+5, 100)
     poke16_(coproc+6, 99)
+do_circles:
 !again:
     poke8_(coproc, 0)
     poke8_(coproc+2, $1)
     poke8_(coproc+1, CCIRCLE)
     trigger_oc()
-    jsr delay
+ 
 #if CLEAR
     poke8_(coproc, 0)
     poke8_(coproc+2, $61)
@@ -125,16 +131,21 @@ do_circles:
     trigger_oc()
 #endif
 
-    //inc coproc+3
-    //inc coproc+3
-    //inc coproc+3
-    //dec coproc+6
-    //dec coproc+6
+    inc coproc+3
+    inc coproc+3
+    inc coproc+3
+    dec coproc+6
+    dec coproc+6
     dec deltmp
     beq !+
     jmp !again-
 !:
     rts
+
+nmi_isr:
+    inc VIC.BgC
+    cli
+    rti
 
 main_entry:
 #if GFXON
@@ -152,22 +163,28 @@ main_entry:
 #endif
     sei
     poke16_($0314, oc_req)
+    poke16_($0318, nmi_isr)
     cli
 
     poke8_(startval, 0)
+    //jsr do_circles_s
+
 !next:
     jsr do_test
     //jsr do_circles
     inc startval
+
+    lda coproc+3
+    adc startval
+    sta coproc+3
+   
 !:
     jsr STD.GETIN
     beq !-
-
     cmp #' '
     beq !+
     jmp !next-
 !:
-
     sei
     poke16_($0314, STD.IRQ)
     cli
